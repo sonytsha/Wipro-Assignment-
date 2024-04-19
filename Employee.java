44048cd812eef01bbd3d75f2342622dce75f79dc
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2024-04-19 15:59:35 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    private String name;
+    private int projectId;
+
+    // Setter and getter for name
+    public void setName(String employeeName) {
+        this.name = employeeName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Setter and getter for projectId
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+}
